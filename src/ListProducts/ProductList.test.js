0b9e723c7114aa1e7abf9b5ef033de6b7d780009
Amit Patel <pamit@thoughtworks.com>
2017-08-14 17:34:32 +0530

    Adding Bootstrap creating a list of products

--- src/ListProducts/ProductList.test.js
+++ src/ListProducts/ProductList.test.js
@@ -4,15 +4,26 @@ import React, { Component } from 'react';
 
 
 it('renders ProductList with list of Products', () => {
-    var container = shallow(<ProductList />);
+  const container = shallow(<ProductList />);
 });
 
-it('should render the products from ProductService', () => {
-  var container = shallow(<ProductList />);
-  expect(container.props().id).toEqual("1");
-});
+// it('should render the products from ProductService', () => {
+//   const container = shallow(<ProductList />);
+//   expect(container.props().id).toEqual("1");
+// });
 
-it('should render the products from ProductService', () => {
-  var container = shallow(<ProductList />);
-  expect(container.props().id).toEqual("1");
-});
+// it( 'should render the products from ProductService', () => {
+//   const container = shallow(<ProductList />);
+//   expect(container.props().id).toEqual("1");
+//   expect(container.find("div[name='productName']").text()).toEqual("Product 1");
+// });
+
+// it( 'should render the product Image is shown from the ProductService', () => {
+//   const container = shallow(<ProductList />);
+//   expect(container.find("img").props().src).toEqual("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLjO4vMxSkwHP4N9i-3FPsbBrJ4oBzYI97LMAHsebHAY_Q_WEL");
+// });
+
+//  it( 'should render the product Name & Description is shown to the right of Image', () => {
+//    const container = shallow(<ProductList />);
+//    expect(container.find("div[name='productDescription']").props().className).toEqual('col-xs-6');
+//  });
