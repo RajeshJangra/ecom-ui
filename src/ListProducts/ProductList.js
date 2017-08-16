82932cbe602fdfb09204fee75c974c112d001e08
Rajesh Kumar <rajeshk@thoughtworks.com>
2017-08-16 15:35:24 +0530

    Product Listing working, with backend data rendered

--- src/ListProducts/ProductList.js
+++ src/ListProducts/ProductList.js
@@ -3,29 +3,41 @@ import { ProductService } from '../Services/ProductService';
 
 class ProductList extends Component {
     products = new ProductService();
+    productsWithCategory=[];
+    productsGrid = [];
+
+    constructor(){
+        super();
+        this.state = { value:[] };
+    }
 
     render(){
-        var productsWithCategory;
-        this.products.getProducts().then(x=>{
-            productsWithCategory = x.products;
-        });
-        var productsGrid = [];
+        return (<div> {this.state.value} </div>);
+    }
 
-        productsWithCategory.forEach(function(product){
-            productsGrid.push(this.showOneProduct(product));
-        }, this);
-
-        return (<div> {productsGrid} </div>);
+    componentDidMount() {
+        var self = this;
+        this.products.getProducts()
+            .then(products=>{
+                self.productsWithCategory = products;
+                self.productsWithCategory.forEach(function(product){
+                    self.productsGrid.push(self.showOneProduct(product));
+                }, this);
+                console.log(2);
+                self.setState({value: self.productsGrid});
+            });
     }
 
 
     showOneProduct(product){
     return (
-    <div id={product.id} className="row">
-        <img name={'productImage' + product.id} className="col-xs-3" src={product.imageUrl}/>
+    <div id={product.id} key={product.id} className="row">
+        <img name={'imageUrl' + product.id} className="col-xs-3" src={product.imageUrl}/>
         <div className="row">
-            <div name={'productName'+ product.id} className="col-xs-6"><b>{product.Name}</b></div>
-            <div name={'productDescription'+product.id} className="col-xs-6"><b>{product.Description}</b></div>
+            <div name={'name'+ product.id} className="col-xs-6"><b>{product.name}</b></div>
+            <div name={'description'+product.id} className="col-xs-6"><b>{product.description}</b></div>
+            <div name={'avalablequantity'+product.id} className="col-xs-6"><b>{product.availableQuantity}</b></div>
+            <div name={'price'+product.id} className="col-xs-6"><b>{product.price}</b></div>
         </div>
     </div>);
     }
