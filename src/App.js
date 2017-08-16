fa477d9f8e800154faac5d001203b9ee53e4b153
Niloy Mondal <niloym@thoughtworks.com>
2017-08-16 11:43:01 +0530

    beautification of landing page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
-import logo from './logo.svg'
+import {Link} from 'react-router-dom';
 import ProductList from './ListProducts/ProductList'
+import logo from './logo.jpg';
+
+const logoStyle = {
+    height: "200px"
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="/login" className="btn btn-primary">Sign in</Link>
-        <Link to="/signup" className="btn btn-primary">Sign up</Link>
-      <div>
-        <ProductList />
-      </div>
-      </  div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container">
+                <h1> Welcome to Titans Ecommerce</h1>
+                <p><img src={logo} style={logoStyle}/></p>
+                <p>
+                    <Link to="/login" className="btn btn-primary col-md-2">Sign in</Link>
+                    <span className="col-md-1"></span>
+                    <Link to="/signup" className="btn btn-primary col-md-2">Sign up</Link>
+                </p>
+
+                <div>
+                    <ProductList/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
